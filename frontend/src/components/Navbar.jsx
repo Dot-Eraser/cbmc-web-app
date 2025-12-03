4c7a665ff55cae72356180b8f3222b2fb797bbe2
Tanmoy Barua <43274420+Tanmoy-Barua@users.noreply.github.com>
2025-12-03 01:35:14 -0800

    update in admin private link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
